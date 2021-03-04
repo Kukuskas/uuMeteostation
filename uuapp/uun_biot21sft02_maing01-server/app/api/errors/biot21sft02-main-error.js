a3f07242b134ec3e20109750ea221a515cebf86f
Jan Bambousek <bambousek.jan@gmail.com>
2021-03-04 16:22:35 +0100

    Initial commit.

---
+++ uuapp/uun_biot21sft02_maing01-server/app/api/errors/biot21sft02-main-error.js
@@ -0,0 +1,43 @@
+"use strict";
+const Biot21sft02MainUseCaseError = require("./biot21sft02-main-use-case-error.js");
+
+const Init = {
+  UC_CODE: `${Biot21sft02MainUseCaseError.ERROR_PREFIX}init/`,
+
+  InvalidDtoIn: class extends Biot21sft02MainUseCaseError {
+    constructor() {
+      super(...arguments);
+      this.code = `${Init.UC_CODE}invalidDtoIn`;
+      this.message = "DtoIn is not valid.";
+    }
+  },
+
+  SchemaDaoCreateSchemaFailed: class extends Biot21sft02MainUseCaseError {
+    constructor() {
+      super(...arguments);
+      this.status = 500;
+      this.code = `${Init.UC_CODE}schemaDaoCreateSchemaFailed`;
+      this.message = "Create schema by Dao createSchema failed.";
+    }
+  },
+
+  SetProfileFailed: class extends Biot21sft02MainUseCaseError {
+    constructor() {
+      super(...arguments);
+      this.code = `${Init.UC_CODE}sys/setProfileFailed`;
+      this.message = "Set profile failed.";
+    }
+  },
+
+  CreateAwscFailed: class extends Biot21sft02MainUseCaseError {
+    constructor() {
+      super(...arguments);
+      this.code = `${Init.UC_CODE}createAwscFailed`;
+      this.message = "Create uuAwsc failed.";
+    }
+  },
+};
+
+module.exports = {
+  Init,
+};
