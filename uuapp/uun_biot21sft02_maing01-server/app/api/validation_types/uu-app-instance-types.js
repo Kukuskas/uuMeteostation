d525f88f6e8ce5f2175911cf32bcf0b680ca98ba
Jan Bambousek <bambousek.jan@gmail.com>
2021-05-11 10:19:41 +0200

    uuAppInstance/scheduleScripts  validation fix

--- uuapp/uun_biot21sft02_maing01-server/app/api/validation_types/uu-app-instance-types.js
+++ uuapp/uun_biot21sft02_maing01-server/app/api/validation_types/uu-app-instance-types.js
@@ -36,7 +36,7 @@ const uuAppInstanceScheduleScriptsDtoInType = shape({
     oneOf(["aggregate","trim","checkGateway"]),
     shape({
       scriptUri: uri().isRequired(),
-      cron: cronExpression(),
+      cron: string(/^(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|(^((?!\*-)((\d+,)+\d+|([\d\*]+(\/|-)\d+)|\d+|(?<!\d)\*(?!\d)) ?){5,7})$/),
       progressCode: code()
     })
   ),
