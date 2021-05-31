3e28088293844e00e7056390cd9c6b35c79d5a1a
Radovan Prochazka <radovan.prochazka@unicorncollege.eu>
2021-05-31 18:02:00 +0200

    tests

--- uuapp/uun_biot21sft02_maing01-server/test/dataset/get.test.js
+++ uuapp/uun_biot21sft02_maing01-server/test/dataset/get.test.js
@@ -1,17 +1,50 @@
 const { TestHelper } = require("uu_appg01_server-test");
 const calls = require("./calls.js");
-const { uuAppInstance } = require("../calls.js");
+const { uuAppInstance, gateway:gatewayCalls } = require("../calls.js");
+const { removeProps } = require("../call-helper");
 
 beforeAll(async () => {
   await TestHelper.setup();
 });
 
+let gateway;
+let dataset;
 beforeEach(async () => {
   await TestHelper.dropDatabase();
   await TestHelper.prepareApp(uuAppInstance.defaults.init);
+  const gatewayCreateDtoIn = {uuEe:"25-8074-1"}
+  gateway = removeProps(await gatewayCalls.create(gatewayCreateDtoIn),["uuAppErrorMap"]);
+  const dtoIn = {
+    data: [{
+      timestamp: "2020-01-01T00:01:23.080Z",
+      temperature: 23.25,
+      humidity: 45.22,
+    }]
+  };
+
+  dataset = removeProps((await gatewayCalls.postData(dtoIn)).data ,["uuAppErrorMap", "gateway"])
 });
 
 afterAll(async () => {
   await TestHelper.teardown();
 });
 
+describe("Testing dataSet/get HDS", () => {
+  test("HDS", async () => {
+    expect.hasAssertions();
+
+    const dtoIn = { gatewayId: gateway.id, // gateway object id
+    type: "detailed", // dataset type
+    date: "2020-01-01"
+    };
+
+    const result = await calls.get(dtoIn)
+
+    
+
+    expect(result.status).toBe(200);
+    expect(result.uuAppErrorMap).toEqual({});
+    expect(result.awid).toBe(TestHelper.getAwid());
+    expect(result.data).toEqual(expect.objectContaining(dataset));
+  });
+});
