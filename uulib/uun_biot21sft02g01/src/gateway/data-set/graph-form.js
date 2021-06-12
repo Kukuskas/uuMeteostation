02b21f869decd439ab386ee09609135f424b2188
Alexandr Kozlov <alexandr.kozlov@seznam.cz>
2021-06-12 18:13:20 +0200

    deploy

--- uulib/uun_biot21sft02g01/src/gateway/data-set/graph-form.js
+++ uulib/uun_biot21sft02g01/src/gateway/data-set/graph-form.js
@@ -35,6 +35,8 @@ export const GraphForm = createVisualComponent({
         setStepSelected("months")
       }else if (params=="yearly") {
         setStepSelected("years")
+      }else{
+        setStepSelected("days")
       }
       
     }
